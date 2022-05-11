4e9961164d49a5433d5feadaf0993dc8b5ebf018
Yassine-Ben-Ghanem <benghanem1999@gmail.com>
2022-05-11 15:52:40 +0100

    first commit

---
+++ app/src/main/java/com/example/androidlibrary/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.androidlibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.toasty.Toaster;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toaster.simpleToaster(this,"hello");
+    }
+}
